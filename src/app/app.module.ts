cfc8f4ad724b70f584a37abc769ecb2b1bbe58f4
Lymanto <lymantohadibrata@gmail.com>
2023-09-19 19:12:36 +0700

    Add TopNav and SelectBox component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
 import { TableComponent } from './component/table/table.component';
+import { TopnavComponent } from './component/topnav/topnav.component';
+import { SelectBoxComponent } from './component/Form/select-box/select-box.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, TableComponent],
+  declarations: [AppComponent, SidebarComponent, TableComponent, TopnavComponent, SelectBoxComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
