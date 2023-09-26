3a33e7758058921702414fe698f9aded3cdb70f3
Vito Jericho <jerichovito@gmail.com>
2023-09-26 21:04:32 +0700

    Updated table

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
-
+ headers = ["No","No. of Request","Entry Date","Dinas","Status","Type of Submission","Status To","Department To","Submission Value","Action"];
 }
