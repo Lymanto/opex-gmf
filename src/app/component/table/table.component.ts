d757f54c020b0c28b220c78fd914a2082f05a762
Glenny Chudra <63528591+typonemma@users.noreply.github.com>
2023-09-27 15:59:01 +0700

    New Updated Table

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivationEnd } from '@angular/router';
+
+export const tabledata = [
+  {
+    src: 'assets/images/icons/ic-arrow-down.svg',
+    srcactive: 'assets/images/icons/ic-arrow-up-active.svg',
+  }
+];
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
+
+
+
 export class TableComponent {
- headers = ["No","No. of Request","Entry Date","Dinas","Status","Type of Submission","Status To","Department To","Submission Value","Action"];
+
+  tabledata = tabledata;
+
+
+  headers = ["No", "No. of Request", "Entry Date", "Dinas", "Status", "Type of Submission", "Status To", "Department To", "Submission Value", "Action"];
+
+  show: boolean = false
+
+  onclick() {
+    this.show = !this.show
+  }
 }
