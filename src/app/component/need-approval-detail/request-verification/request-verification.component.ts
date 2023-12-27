89460476923f82d10510306d02f3aa0e5a8e6ce1
Lymanto <lymantohadibrata@gmail.com>
2023-12-27 14:47:18 +0700

    need approval detail integration

--- src/app/component/need-approval-detail/request-verification/request-verification.component.ts
+++ src/app/component/need-approval-detail/request-verification/request-verification.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { EMPTY, Subject, catchError, takeUntil, tap } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { format } from 'date-fns';
+import { EMPTY, Subject, catchError, takeUntil } from 'rxjs';
+import { RealizationDTO } from 'src/app/dto/request-verification.dto';
 import { RequestVerificationService } from 'src/app/services/opex/need-approval/request-verification.service';
 
 @Component({
@@ -8,43 +9,12 @@ import { RequestVerificationService } from 'src/app/services/opex/need-approval/
   templateUrl: './request-verification.component.html',
   styleUrls: ['./request-verification.component.css'],
 })
-export class RequestVerificationComponent {
-  id: number;
-  constructor(
-    private requestVerification: RequestVerificationService,
-    private readonly route: ActivatedRoute
-  ) {
-    const _id: number = parseInt(
-      this.route.snapshot.paramMap.get('id') as string
-    );
-    this.id = _id;
+export class RequestVerificationComponent implements OnInit {
+  @Input() data!: RealizationDTO;
+  ngOnInit(): void {
+    console.log(this.data);
   }
-  private readonly _onDestroy$: Subject<void> = new Subject<void>();
-
-  ngOnDestroy(): void {
-    this._onDestroy$.next();
-    this._onDestroy$.complete();
-  }
-
-  ngOnInit() {
-    this.fetchRequestVerification();
-  }
-
-  fetchRequestVerification(): void {
-    this.requestVerification
-      .getRequestVerification(this.id)
-      .pipe(
-        catchError((err) => {
-          console.error('Error occurred:', err);
-          return EMPTY;
-        }),
-        tap((result) => {
-          if (result && result.data) {
-            const allData = result.data.flatMap((item) => item);
-          }
-        }),
-        takeUntil(this._onDestroy$)
-      )
-      .subscribe();
+  formatDate(val: Date): string {
+    return format(new Date(val), 'dd MMM yyyy');
   }
 }
